7612ccd9bcdbca52401b011428cd8a80160f69cd
cdltlehf <cdltlehf@naver.com>
2020-06-20 15:32:12 +0900

    fix fetch

--- backend/client/src/components/home.component.js
+++ backend/client/src/components/home.component.js
@@ -2,55 +2,6 @@ import React, { Component } from "react";
 import { LineChart, XAxis, Tooltip, CartesianGrid, Line, ResponsiveContainer } from "recharts";
 
 export default class extends Component {
-  fetchData() {
-    fetch('/api/get_data', {
-      method: 'post',
-      headers: {'Content-Type':'application/json'},
-      body: JSON.stringify({ email: this.props.token })
-    })
-    .then(res => res.json())
-    .then(json => {
-      this.setState(prevState => ({ ...prevState, ...json }));
-    });
-  }
-
-  fetchGraphData() {
-    fetch('/api/get_graph_data', {
-      method: 'post',
-      headers: {'Content-Type':'application/json'},
-      body: JSON.stringify({ email: this.props.token })
-    })
-    .then(res => res.json())
-    .then(json => {
-      this.setState(json);
-    });
-  }
-
-  fetchRivalGraphData(email) {
-    fetch('/api/get_graph_data', {
-      method: 'post',
-      headers: {'Content-Type':'application/json'},
-      body: JSON.stringify({ email })
-    })
-    .then(res => res.json())
-    .then(json => {
-      console.log(json.graph_data);
-      this.setState(prevState => ({ ...prevState, rival_graph_data: json.graph_data }));
-    });
-  };
-
-  fetchLeaderBoard() {
-    fetch('/api/get_leaderboard', {
-      method: 'post',
-      headers: {'Content-Type':'application/json'},
-      body: JSON.stringify({ email: this.props.token })
-    })
-    .then(res => res.json())
-    .then(json => {
-      json.leaderboard.sort((a, b) => a.score_rank - b.score_rank);
-      this.setState(json);
-    });
-  }
   constructor(props) {
     super(props);
     this.state = { 
@@ -68,13 +19,64 @@ export default class extends Component {
 
   componentDidMount() {
 
-    this.fetchData();
-    this.fetchGraphData();
-    this.fetchLeaderBoard();
+    const fetchData = () => {
+      fetch('/api/get_data', {
+        method: 'post',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({ email: this.props.token })
+      })
+      .then(res => res.json())
+      .then(json => {
+        this.setState(prevState => ({ ...prevState, ...json }));
+      });
+    }
 
-    setInterval(this.fetchData, 1000);
-    setInterval(this.fetchGraphData, 5000);
-    setInterval(this.fetchLeaderBoard, 5000);
+    const fetchGraphData = () => {
+      fetch('/api/get_graph_data', {
+        method: 'post',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({ email: this.props.token })
+      })
+      .then(res => res.json())
+      .then(json => {
+        this.setState(json);
+      });
+    }
+
+    const fetchRivalGraphData = (email) => {
+      fetch('/api/get_graph_data', {
+        method: 'post',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({ email })
+      })
+      .then(res => res.json())
+      .then(json => {
+        this.setState(prevState => ({ ...prevState, rival_graph_data: json.graph_data }));
+      });
+    };
+
+    this.props.fetchRivalGraphData = fetchRivalGraphData;
+
+    const fetchLeaderBoard = () => {
+      fetch('/api/get_leaderboard', {
+        method: 'post',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({ email: this.props.token })
+      })
+      .then(res => res.json())
+      .then(json => {
+        json.leaderboard.sort((a, b) => a.score_rank - b.score_rank);
+        this.setState(json);
+      });
+    }
+
+    fetchData();
+    fetchGraphData();
+    fetchLeaderBoard();
+
+    setInterval(fetchData, 1000);
+    setInterval(fetchGraphData, 5000);
+    setInterval(fetchLeaderBoard, 5000);
   }
 
   render() {
@@ -131,7 +133,7 @@ export default class extends Component {
             </thead>
             <tbody>
               { this.state.leaderboard.map((e, i) => 
-                <tr key={i} onClick={() => { this.fetchRivalGraphData(e.email); }}>
+                <tr key={i} onClick={() => { this.props.fetchRivalGraphData(e.email); }}>
                   <td id="ranking">{i+1}</td>
                   <td id="nickname">{e.username}</td>
                   <td id="score">{Math.floor(e.score)}</td>
